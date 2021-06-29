f17b7ce63f578c9582b079d17b0496b689bfd661
Sara Fishburn <fishburn.sara@gmail.com>
2021-06-29 10:04:28 +1000

    made a working clock

--- src/App.js
+++ src/App.js
@@ -1,16 +1,56 @@
-import React from 'react';
-import Clock from "./Clock";
+import React, { Component } from "react"
+import Clock from "./Clock"
 
-const App = (props) => {
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      latitude: null,
+      error: null,
+      date: new Date(),
+      timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+      icon: 'sun.svg'
+    }
+  }
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ latitude: position.coords.latitude }),
+      (error) => this.setState({ error: error.message })
+    )
+
+    this.timerId = setInterval(() => {
+      this.setState({ date: new Date() })
+    }, 1000)
+  }
+
+  componentDidUpdate(_prevProps, prevState) {
+    if(this.state.date !== prevState.date) {
+      let hours = this.state.date.getHours()
+      this.setState({ icon: hours > 18 || hours < 4 ? 'snowflake.svg' : 'sun.svg' })
+    }
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerId)
+  }
+
+  render() {
     return (
-        <div>
-            <Clock 
-                icon="sun.svg"
-                timezone={"Sydney/Australia"} 
-                date={new Date()} 
+      <div>
+        {this.state.error ? (
+          <p style={{ color: 'red' }}>{this.state.error}</p>
+        ) : (
+          <>
+            <h4>{this.state.latitude}</h4>
+            <Clock
+              icon={this.state.icon}
+              timezone={this.state.timezone}
+              date={this.state.date}
             />
-        </div>
-    );
+          </>
+        )}
+      </div>
+    )
+  }
 }
-
-export default App;
